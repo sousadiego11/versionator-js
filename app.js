61915e2d043256a43a46b1aa36421db251421d95
Diego <diegodev.geomais@gmail.com>
2022-03-24 11:59:13 -0300

    feat: add author and date (#TEST)

--- app.js
+++ app.js
@@ -27,16 +27,18 @@ const mdCreator =  {
     chore(e) {
         this.chores.push(e)
     },
-    build({ body, tag, issue }) {
-        return  issue ? `[${issue}](${config}/${tag}) - ${body} ${config}/${tag}\n` : `${body} ${config}/${tag}\n`
+    build({ body, tag, issue, date, author }) {
+        return  issue ? `${author}: [${issue}](${config}/${tag}) - ${body} (${date}) ${config}/${tag}\n` : `${author}: ${body} (${date}) ${config}/${tag}\n`
     }
 }
 
 
 const currentChangelog = fs.existsSync(mdDir);
-const output = child.execSync('git log --format=%B%H--DELIMITER--').toString().split('--DELIMITER--\n')
 
-const changelog = `\n# Versão ${version}\n\n`
+// const output = child.execSync('git log --format=%B%H--DELIMITER--').toString().split('--DELIMITER--\n')
+const output = child.execSync('git log --format=date={%as}author={%an}%B%H--DELIMITER--').toString().split('--DELIMITER--\n')
+
+const changelog = `\n# Versão ${version}\n`
 
 if (!currentChangelog) {
     fs.writeFileSync(mdDir, changelog)
@@ -45,21 +47,27 @@ if (!currentChangelog) {
     fs.appendFileSync(mdDir, changelog)
 }
 
+const execRegex = (validator, regex) => {
+    return validator && regex ? regex : []
+} 
+
 const commits = output.map((c) => {
-    const [body, tag] = c.split('\n')
+    const [bodyRaw, tag] = c.split('\n')
 
-    const type = tag && /(feat|refactor):/.exec(body) ? /(feat|refactor):/.exec(body) : []
-    const issue = tag && /\(#(.+)\)/.exec(body) ? /\(#(.+)\)/.exec(body) : []
-
-    return {body, tag, type: type[1], issue: issue[1]}
+    const type = execRegex(tag, /(feat|refactor):/.exec(bodyRaw))
+    const issue = execRegex(tag, /\(#(.+)\)/.exec(bodyRaw))
+    const date = execRegex(tag,/date={(.+?)}/.exec(bodyRaw))
+    const author = execRegex(tag,/author={(.+?)}/.exec(bodyRaw))
+    
+    const body = bodyRaw.replace(type[0], '').replace(issue[0], '').replace(date[0], '').replace(author[0], '')
+    
+    return {body, tag, type: type[1], issue: issue[1], date: date[1], author: author[1]}
     
 }).filter(i => i.tag)
 
 commits.forEach((c) => mdCreator[c.type](mdCreator.build(c)))
 const content = mdCreator.feats.join('\n')
 
-// fs.appendFileSync(mdDir, content)
-const fb = fs.openSync(mdDir, 'a+')
-fs.writeSync(fb, content, 0, content.length, 0)
+fs.writeSync(fs.openSync(mdDir, 'a+'), content, 0, content.length, 0)
 
 console.table(commits)
