572dadd91bf9398f4dc924e83790ae12fa0e2ab0
Diego <diegodev.geomais@gmail.com>
2022-04-08 08:35:50 -0300

    fix: get string from stdout buffer

--- src/utils/getUrlGitRepo.js
+++ src/utils/getUrlGitRepo.js
@@ -1,10 +1,10 @@
 import { execSync } from 'child_process';
 
 const getUrlGitRepo = async () => {
-    const { stdout } = execSync('git ls-remote --get-url');
+    const stdout = execSync('git ls-remote --get-url');
     if (stdout) {
         // Transform git url to https url (is needed to create the url accessible by the browser)
-        return stdout.replace(/\n/g, '').replace('git@', 'https://').replace('com:', 'com/').replace('.git', '') + '/commits';
+        return stdout.toString().replace(/\n/g, '').replace('git@', 'https://').replace('com:', 'com/').replace('.git', '') + '/commits';
     }
     return;
 };
