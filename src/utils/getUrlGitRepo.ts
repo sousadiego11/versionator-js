354023f17cae73f361882c2d02ff1383716320e1
Diego Sousa <diegointeriores11@gmail.com>
2022-04-21 19:09:18 -0300

    feat: default issues dir

--- src/utils/getUrlGitRepo.ts
+++ src/utils/getUrlGitRepo.ts
@@ -1,10 +1,10 @@
 import { execSync } from 'child_process'
 
-const getUrlGitRepo = (): string => {
+const getUrlGitRepo = (target: string): string => {
   const stdout = execSync('git ls-remote --get-url')
   if (stdout) {
     // Transform git url to https url (is needed to create the url accessible by the browser)
-    return stdout.toString().replace(/\n/g, '').replace('git@', 'https://').replace('com:', 'com/').replace('.git', '') + '/commits'
+    return stdout.toString().replace(/\n/g, '').replace('git@', 'https://').replace('com:', 'com/').replace('.git', '') + `/${target}`
   }
   return ''
 }
--- src/utils/promisedReadline.ts
+++ src/utils/promisedReadline.ts
@@ -13,7 +13,9 @@ type Config = {
 }
 
 const promisedReadline = async (cfgDir: PathOrFileDescriptor) => {
-  const urlToValidateGit = getUrlGitRepo()
+  const commitsUrl = getUrlGitRepo('commits')
+  const issuesUrl = getUrlGitRepo('issues')
+
   return await new Promise((resolve) => {
 
     const configs: Config = {
@@ -26,14 +28,14 @@ const promisedReadline = async (cfgDir: PathOrFileDescriptor) => {
       output: process.stdout
     })
 
-    rl.question(black.bgYellow.bold('URL for your repository commits \n') + `Confirm the detected URL: "${urlToValidateGit}" - or type the URL: `, (commits_dir) => {
-      configs.commits_dir = commits_dir || urlToValidateGit
+    rl.question(black.bgYellow.bold('URL for your repository COMMITS \n') + `Confirm the detected URL: "${commitsUrl}" - or type the URL: `, (commits_dir) => {
+      configs.commits_dir = commits_dir || commitsUrl
 
-      rl.setPrompt(black.bgYellow.bold('URL for your repository issues:'))
+      rl.setPrompt(black.bgYellow.bold('URL for your repository ISSUES \n') + `Confirm the detected URL: "${issuesUrl}" - or type the URL: `)
       rl.prompt()
 
       rl.on('line', (issue) => {
-        configs.issues_dir = issue
+        configs.issues_dir = issue || issuesUrl
         rl.close()
       })
 
